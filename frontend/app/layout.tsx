2faf8f06bb24b5db81e6792542c448355f295b76
Edon Abdullahu <edonabdullahu2@gmail.com>
2025-12-29 11:39:50 +0100

    fix: add script for seo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Fira_Code } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const firaCode = Fira_Code({
   subsets: ['latin'],
@@ -67,7 +68,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={firaCode.variable}>
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXX');
+          `}
+        </Script>
+        <Script
+          src="https://plausible.io/js/script.js"
+          data-domain="gitlearner.com"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   )
 }
