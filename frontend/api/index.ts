fb9c9f739bfeea1381278bebe86a1f1578ea26d7
Edon Abdullahu <edonabdullahu2@gmail.com>
2026-01-11 14:54:58 +0100

    removed comment

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,24 +1,5 @@
 import axios from 'axios'
 
-// export const axiosInstance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   withCredentials: true,
-// })
-
-// axiosInstance.interceptors.request.use((config) => {
-//   if (typeof window !== 'undefined') {
-//     const token = localStorage.getItem('token')
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-//   }
-//   return config
-// })
-
-// export default axiosInstance
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
